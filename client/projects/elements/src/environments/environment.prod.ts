6f7110601ef142b1c498fdc7ceb3864f9db88dde
Shawn Partridge <shawn.partridge@gmail.com>
2020-07-13 15:23:00 -0700

    Removed Dist

--- client/projects/elements/src/environments/environment.prod.ts
+++ client/projects/elements/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ydh: {
-    baseUrl: 'https://us-central1-your-dog-here.cloudfunctions.net/',
+    baseUrl: '',
   },
 };
--- client/projects/elements/src/environments/environment.ts
+++ client/projects/elements/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   ydh: {
-    baseUrl: 'https://us-central1-your-dog-here.cloudfunctions.net/',
+    baseUrl: '',
   },
 };
 
